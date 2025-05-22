a3927a01a9d5f80b150980f5473539410fc7a69b
biyash-1 <sthaalish39@gmail.com>
2025-05-22 23:55:52 +0545

    dash

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const page = () => {
+const page = ({prop}) => {
   return (
     <div>
       <h1>hi! im  dashboard</h1>
+      <p>this is dashboard page</p>
     </div>
   )
 }
